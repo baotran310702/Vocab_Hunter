474d03e0af4ce69c89bf9aa76f159d1e88f11545
baotran310702 <tranbao1414@gmail.com>
2024-03-09 19:15:19 +0700

    update new change

--- lib/utils/word_similarity.dart
+++ lib/utils/word_similarity.dart
@@ -1,6 +1,3 @@
-import 'package:tflite_flutter/tflite_flutter.dart';
-import 'package:tflite_v2/tflite_v2.dart';
-
 class WordSimilarityML {
   final String model = "assets/glove_model.tflite";
   final String labels = "assets/labels.txt";
